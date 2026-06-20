40fafdc3b3e70945f4a30db28ee2f1520334abeb
JoseMasiaTormo <josemasia2004@gmail.com>
2026-06-20 14:48:02 +0200

    db conexion created

---
+++ tasks/db.php
@@ -0,0 +1,12 @@
+<?php 
+function getDB() {
+    static $db = null;
+
+    if ($db === null) {
+        $db = new PDO('sqlite:' . __DIR__ . '/tasks.db');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    return $db;
+}
+?>
